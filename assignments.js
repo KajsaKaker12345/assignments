a92ff9d69d2d219b64be35a69b46999924b811aa
Kajsa Kaker <kk223ic@student.lnu.se>
2026-02-02 21:38:39 +0100

    add html och Js information

---
+++ assignments.js
@@ -0,0 +1,6 @@
+const assignments = {
+    id: "assignment1",
+    title: "Uppgift 1 - Samlingssida för inlämningsuppgifter",
+    link: "",
+    description: "Skapa en samlingsida för alla inlämningsuppgifter som kommer genom kursen"
+}
