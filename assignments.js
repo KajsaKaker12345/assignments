d5dfa9ebe5202bec17acd79c53360fe0a1ca37ed
Kajsa Kaker <kk223ic@student.lnu.se>
2026-02-03 11:17:18 +0100

    Add more html and JS

--- assignments.js
+++ assignments.js
@@ -1,6 +1,14 @@
-const assignments = {
+const assignments = [
+    {
     id: "assignment1",
     title: "Uppgift 1 - Samlingssida för inlämningsuppgifter",
     link: "",
     description: "Skapa en samlingsida för alla inlämningsuppgifter som kommer genom kursen"
+},
+{
+    id: "assignment2",
+    title: "Uppgift 2 - Produktsida med kundvagn",
+    link: "",
+    description: "Skapa en produktsida som dynamiskt renderar produkter från datastruktur"
 }
+];
