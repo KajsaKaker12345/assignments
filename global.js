9b671832b54640b61d75d5aedf78baa362ff01b3
Kajsa Kaker <kk223ic@student.lnu.se>
2026-02-04 13:30:49 +0100

    add info uppgift1

--- global.js
+++ global.js
@@ -5,12 +5,19 @@ export function createNavigation() {
 const globalList = document.getElementById("list");
 
 if (!globalList) return;
+const currentPage = document.body.dataset.page;
 
 for (const a of assignments) {
 const li = document.createElement("li")
 const link = document.createElement("a")
 link.href = a.link;
 link.textContent = a.title;
+link.dataset.id = a.id;
+
+if (a.id === currentPage) {
+    link.classList.add("active");
+    li.classList.add("active")
+}
 
 li.append(link);
 globalList.append(li);
--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 import {createNavigation} from "./global.js";
 import { assignments } from "./assignments.js";
 console.log(createNavigation);
+
+const container = document.getElementById("assignment");
+const assignment = assignments.find(assignments => assignments.id === "assignment1" );
+const h1 = document.createElement("h1");
+const p = document.createElement("p");
+const link = document.createElement("a");
+
+h1.textContent = assignment.title;
+p.textContent = assignment.description;
+link.href = "../index.html";
+link.textContent = "Tillbaka till startsidan!"
+container.append(h1, p, link );
