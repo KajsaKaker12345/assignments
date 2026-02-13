b9b4ed3a69fbca81c735c857263bc756b0b09816
Kajsa Kaker <kk223ic@student.lnu.se>
2026-02-13 11:33:28 +0100

    add change

--- assignment2/cart.js
+++ assignment2/cart.js
@@ -1,5 +1,19 @@
-
+const number = document.getElementById("h4");
+const container = document.getElementById("section");
+const btn = document.getElementById("clear");
 export const cart = [];
+
+// spara kundkorgen
+const saved = localStorage.getItem("cart");
+if (saved) {
+    const parsed = JSON.parse(saved);
+    
+    for (const p of parsed) {
+        cart.push(p);
+    }
+    makeCart();
+}
+// lägg till i korgen
 export function addToCart(product) {
     const clickedProduct = cart.find(p => p.id === product.id)
     
@@ -9,14 +23,14 @@ export function addToCart(product) {
     else {
     cart.push(product)
     }
+    saveCart();
     makeCart();
 }
+// skapa korg
 function makeCart() {
-    const container = document.getElementById("section");
 
     container.innerHTML = "";
     let total = 0;
-    const number = document.getElementById("h4")
 
     for (const c of cart) {
     total += c.price * c.count;
@@ -24,7 +38,23 @@ function makeCart() {
         const p = document.createElement("p");
         p.textContent = `${c.name}, ${c.price}kr, Antal:${c.count}`
         container.append(p);
-        
-    number.innerHTML = `Total: ${total}kr`;
     }
+    number.innerHTML = `Total: ${total}kr`;
 }
+//spara innehållet korgen
+function saveCart() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+//Töm hela varukorgen 
+btn.addEventListener("click", () => {
+    
+    for (const c of cart) {
+        c.count = 1;
+    }
+    cart.length = 0;
+    localStorage.removeItem("cart");
+    makeCart();
+    container.innerHTML = "";
+    number.innerHTML = "Total: 0";
+});
--- assignment2/main.js
+++ assignment2/main.js
@@ -1,18 +1,6 @@
-import { cart } from "./cart.js";
+
 import { createNavigation } from "../global.js";
 import { createCards } from "./productCard.js";
 createNavigation(true);
 createCards();
 
-const number = document.getElementById("h4");
-const container = document.getElementById("section");
-const btn = document.getElementById("clear");
-btn.addEventListener("click", () => {
-    
-    for (const c of cart) {
-        c.count = 1;
-    }
-    cart.length = 0;
-    container.innerHTML = "";
-    number.innerHTML = "Total: 0";
-})
