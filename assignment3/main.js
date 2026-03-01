471f504bbf025963e922e6913f0ebaac805b4c57
Kajsa Kaker <kk223ic@student.lnu.se>
2026-03-01 11:02:20 +0100

    add function data

--- assignment3/main.js
+++ assignment3/main.js
@@ -1,10 +1,20 @@
 import { createNavigation } from "../global.js";
-import { Match } from "./match.js";
-
 createNavigation(true);
+import { Match } from "./match.js";
 
 fetch("contestants.json")
 .then(response => response.json())
 .then(data => {
-    console.log(data);
+
+    const container = document.getElementById("turneringen");
+    const round = document.createElement("div");
+    round.classList.add("round");
+
+    for (let i = 0; i < data.length; i += 2) {
+    const match = new Match(data[i], data[i + 1]);
+    round.append(match.makeHTML());
+   
+
+}
+container.append(round);
 });
