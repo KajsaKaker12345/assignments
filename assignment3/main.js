25d9cd4dc9a6faba85f7f99a7f0911b97b784dc9
Kajsa Kaker <kk223ic@student.lnu.se>
2026-03-03 23:03:25 +0100

    add playing structure+button

--- assignment3/main.js
+++ assignment3/main.js
@@ -1,20 +1,51 @@
 import { createNavigation } from "../global.js";
 createNavigation(true);
-import { Match } from "./match.js";
+import { createRounds } from "./round.js";
+let playingMatch = [];
+const names = ["Kvartsfinal", "Semifinal", "Final"];
+let currentRound = 0;
+startGame();    
 
+// Hämta contestants från fil json
+function startGame() {
 fetch("contestants.json")
-.then(response => response.json())
-.then(data => {
+    .then(response => response.json())
+    .then(data => {
 
+
+      // skapa första matcher med datan  
+        playingMatch = createRounds(data, "Kvartsfinal");
+        currentRound = 0;
+
+    });}
+
+    // knapp för att visa winner och skapa ny lista och anropa createRounds
+const button = document.getElementById("btn");
+button.addEventListener("click", () => {
+    for (const match of playingMatch) {
+        match.compete();
+    }
+    currentRound++;
+    const winners = playingMatch.map(m => m.winner)
+    playingMatch = createRounds(winners, names[currentRound]);
+
+    if (playingMatch.length === 2) {
+        button.textContent = "Simulera Semifinal"
+    } 
+    if (playingMatch.length === 1) {
+        button.textContent = "Simulera Final"
+    } else if (playingMatch.length === 0) {
+        button.hidden = true;
+        roundName = "";
+    }
+});
+    // restartknapp
+const restart = document.getElementById("startbtn");
+restart.addEventListener("click", () => {
+    button.hidden = false;
     const container = document.getElementById("turneringen");
-    const round = document.createElement("div");
-    round.classList.add("round");
+    container.innerHTML = "";
+    button.textContent = "Simulera Kvartsfinal"
 
-    for (let i = 0; i < data.length; i += 2) {
-    const match = new Match(data[i], data[i + 1]);
-    round.append(match.makeHTML());
-   
-
-}
-container.append(round);
+    startGame();
 });
